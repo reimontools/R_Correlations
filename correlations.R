e1341b0d9df880b3aadb2b5deaadb4190633ecd8
Solange Parra <2489076p@student.gla.ac.uk>
2020-10-26 14:56:38 +0000

    Correlation example
    I created the file

---
+++ correlations.R
@@ -0,0 +1,14 @@
+print("correlations")
+library(corrplot)
+head(mtcars)
+M<-cor(mtcars)
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+corrplot(M, method="color", col=col(200),  
+         type="upper", order="hclust", 
+         addCoef.col = "black", # Add coefficient of correlation
+         tl.col="black", tl.srt=45, #Text label color and rotation
+         # Combine with significance
+         p.mat = p.mat, sig.level = 0.01, insig = "blank", 
+         # hide correlation coefficient on the principal diagonal
+         diag=FALSE 
+)
